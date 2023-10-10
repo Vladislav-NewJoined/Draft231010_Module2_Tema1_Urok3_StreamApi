3871e31885f89809cc6301e2986040b76fd84eab
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-10-10 19:23:40 +0300

    Initial commit

--- src/Draft_Task2_1_3_0_Whole_Video.java
+++ src/Draft_Task2_1_3_0_Whole_Video.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.stream.IntStream;
+
+public class Draft_Task2_1_3_0_Whole_Video {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+        int summary = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                summary += array[i];
+            }
+        }
+        System.out.println(summary);
+
+        System.out.println(IntStream.of(array).filter((a) -> a > 0).sum());
     }
 }
